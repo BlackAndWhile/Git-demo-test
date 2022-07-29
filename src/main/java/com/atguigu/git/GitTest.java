a24fc95ab3f7c963af56c00daf1585bfb2ae3872
Lzq <Lzq@guigu.com>
2022-07-29 22:36:09 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("Hello, git4!!!!");
         System.out.println("master");
         System.out.println("hot fix");
+        System.out.println("push test");
     }
 
 }
